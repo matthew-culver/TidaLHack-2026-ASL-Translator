4509f63d3f0a325fc45c83183aaa25d7af022d93
matthew-culver <matthewkarlculver@gmail.com>
2026-02-07 13:22:25 -0600

    fixed filepath for seed

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const SignVocabulary = require('models/SignVocabulary');
+const SignVocabulary = require('./models/SignVocabulary');
 
 const signs = [
   {
